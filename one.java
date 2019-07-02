6c01ac537d456b5406ce64aa6af4270be7e1efea
Biswabijaya Samal <me@biswabijaya.com>
2019-07-02 10:23:19 +0530

    Overloading

---
+++ one.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.string;
+public static void main(String[] args) {
+  tostring t1 = new toString();
+  tostring t2 = new toString();
+
+  System.out.println("Instance Created");
+}
+public String tostring() {
+  System.out.println("Instance Created");
+}
